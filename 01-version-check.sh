56969c88259427eadf20f3fdb6f24ee7299a9f0f
Jansen Price <sumpygump@gmail.com>
2022-01-27 02:40:57 -0600

    Add boilerplate banners to main scripts

--- 01-version-check.sh
+++ 01-version-check.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-# Simple script to list version numbers of critical development tools
+
+#----------------------------------------------------------------------------#
+#  __    _____ _____
+# |  |  |   __|   __|
+# |  |__|   __|__   |
+# |_____|__|  |_____|
+#
+# Automate LFS Build
+#----------------------------------------------------------------------------#
+# (C) 2019-2022 Ranjith Gowda, Jansen Price
+# https://github.com/ranjithum/automate-lfs-build
+#----------------------------------------------------------------------------#
+# Usage: ./01-version-check.sh
+# This script will perform some preliminary checks to determine if the host
+# machine has the ncessary tools to build the remaining toolchain and packages.
+# If a required program does not exist, it will show as an error. Otherwise it
+# will print the current version. It is up to the user to visually check the
+# versions from the output of each program compared to the minimum requirements
+# for lfs.
+#----------------------------------------------------------------------------#
+
+REPO_ROOT=$(cd "$(dirname "${BASH_SOURCE[1]}")" &> /dev/null && pwd)
+. $REPO_ROOT/common/common_utils
 
 # -----------------
 # Utility functions
 # -----------------
-setup_terminal() {
-    TPUT_RESET=""
-    TPUT_RED=""
-    TPUT_GREEN=""
-    test -t 1 || return 1 # abort if not a terminal
-    if command -v tput >/dev/null 2>&1; then
-        if [ $(($(tput colors 2>/dev/null))) -ge 8 ]; then
-            TPUT_RESET="$(tput sgr 0)"
-            TPUT_RED="$(tput setaf 1)"
-            TPUT_GREEN="$(tput setaf 2)"
-        fi
-    fi
-    return 0
-}
-success() {
-    printf "${TPUT_GREEN}${*}${TPUT_RESET}\n"
-}
-error() {
-    printf "${TPUT_RED}ERROR: ${*}${TPUT_RESET}\n"
-}
 assert_has() {
     check_label=$([ -n "$2" ] && echo "$2" || echo "$1")
     printf "Checking for $check_label ... "
@@ -39,7 +41,6 @@ assert_has() {
 # -----------------
 # Main
 # -----------------
-setup_terminal || echo >/dev/null
 
 HAS_ERROR=0
 export LC_ALL=C
@@ -64,7 +65,7 @@ if [ -h /usr/bin/yacc ]; then
 elif [ -x /usr/bin/yacc ]; then
     success "yacc is $(/usr/bin/yacc -V | head -n1)"
 else
-    error "yacc not found" 
+    error "yacc not found"
 fi
 
 assert_has bzip2 && success $(bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-)
@@ -78,8 +79,8 @@ if [ -h /usr/bin/awk ]; then
     success "/usr/bin/awk -> $(readlink -f /usr/bin/awk)"
 elif [ -x /usr/bin/awk ]; then
     success "awk is $(/usr/bin/awk --version | head -n1)"
-else 
-    error "awk not found" 
+else
+    error "awk not found"
 fi
 
 HAS_GPP=0
@@ -99,7 +100,7 @@ assert_has tar && success $(tar --version | head -n1)
 assert_has makeinfo texinfo && success $(makeinfo --version | head -n1)
 assert_has xz && success $(xz --version | head -n1)
 
-if [ $HAS_GPP -eq 1 ]; then 
+if [ $HAS_GPP -eq 1 ]; then
     printf "Checking g++ compilation ... "
     echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
     if [ -x dummy ]
